eca87446faef469b1c395901537833a620f3dd7b
Julian Bohorquez <julianarmando@gmail.com>
2013-12-18 04:43:47 -0500

    Codigo simplificado para empezar a hacer las modificaciones correspondientes

--- index.php
+++ index.php
@@ -4,15 +4,19 @@ $ciclos = "SELECT ciclo_escolar FROM ciclos ORDER BY ciclo_escolar asc";
 $res_ciclo= mysqli_query($conexion, $ciclos);
 ?>
 
-<tr>
-	<td align="right">
-		Ciclo Escolar:
-		<select name="select" id="txtCiclo">
-<?php
-		while ($result = mysqli_fetch_assoc($res_ciclo)){
-			echo "<option value='".$result['ciclo_escolar']."'> ".$result['ciclo_escolar']."</option>";
-		}
-?>
-		</select>
-	</td>
-</tr>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<select name="select" id="txtCiclo">
+	<?php
+	while ($result = mysqli_fetch_assoc($res_ciclo)){
+		echo "<option value='".$result['ciclo_escolar']."'> ".$result['ciclo_escolar']."</option>";
+	}
+	?>
+</select>
+</body>
+</html>
